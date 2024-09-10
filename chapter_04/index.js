2a049406defb3220ff40cb315a5d6ff38a81606b
muhammad saad <studentsaad41@gmail.com>
2024-09-10 21:09:19 +0500

    updated

---
+++ chapter_04/index.js
@@ -0,0 +1,22 @@
+// QUESTION #1
+var x, y, z;
+console.log(typeof x, y, z);
+
+// QUESTION #2
+var fullName, name1, _score, $signup, title$1; //legal variables name
+// var 123, 1abc, @lastName, title&, p!roduct; //illegal variables name
+
+// QUESTION #3
+var heading = "Rules for naming JS variables";
+document.getElementById("heading").innerText = heading;
+
+var para1, para2, para3, para4;
+para1 = "variable names can only contain , numbers, $ and _. For example: $my_1stVariable";
+para2 = "variable must begin with a letter, $ or _. For example : $name, _name or name";
+para3 = "variable names are case sensitive";
+para4 = "variable names should not be JS keywords";
+document.getElementById("para1").innerText = para1;
+document.getElementById("para2").innerText = para2;
+document.getElementById("para3").innerText = para3;
+document.getElementById("para4").innerText = para4;
+
---
+++ chapter_05/index.js
@@ -0,0 +1,26 @@
+// QUESTION 1
+var num1 = 30, num2 = 50;
+var numNew = num1 + num2;
+document.write(`<h3>Sum of ${num1} and ${num2} is ${numNew}</h3>`);
+// document.write("Sum of " + num1 + " and " + num2 + " is " + numNew );
+
+// QUESTION 2
+var num1 = 100, num2 = 50;
+var numNew = num1 - num2;
+document.write(`<h3>Subtract ${num1} from ${num2} is ${numNew}</h3>`);
+
+var num1 = 40, num2 = 10;
+var numNew = num1 * num2;
+document.write(`<h3>Multiply ${num1} from ${num2} is ${numNew}</h3>`);
+
+var num1 = 1400, num2 = 5;
+var numNew = num1 / num2;
+document.write(`<h3>${num1} divided by ${num2} is ${numNew}</h3>`);
+
+var num1 = 100, num2 = 90;
+var numNew = num1 % num2;
+document.write(`<h3>Modulus ${num1} of ${num2} is ${numNew}</h3>`);
+
+// QUESTION 3
+var newVariable;
+document.write(`<h3>Value after variable declaration is: ${newVariable}</h3>`);
