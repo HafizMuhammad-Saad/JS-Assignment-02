d2f27913e7853b9a72399634bd23512186ba52a0
muhammad saad <studentsaad41@gmail.com>
2024-09-15 21:52:23 +0500

    updated

--- chapter_12-13/index.js
+++ chapter_12-13/index.js
@@ -1,41 +1,92 @@
-// var char = prompt("Enter a chrachter: ");
-// var asciicode = char.charCodeAt(0);
+// QUESTION NO # 1
+// let char = prompt("Enter a character");
+// let result = char.charCodeAt(0);
 
-// if (asciiCode >= 48 && asciiCode <= 57) {
-//     document.write("The character is a number.")
-// } else if (asciiCode >= 65 && asciiCode <= 90) {
-//     document.write("The character is an uppercase letter.")
-// } else if (asciiCode >= 97 && asciiCode <= 122) {
-//     document.write("The character is a lowercase letter.")
+// if(result >= 48 && result <= 57) {
+//     alert("This is a Number");
+// } else if(result >= 65 && result <= 90) {
+//     alert("This is a string and Uppercase");
+
+// } else if(result >= 97 && result <= 122) {
+//     alert("This is a string and Lovercase");
 // } else {
-//     document.write("The character is not a number or letter.")
-// };
-
-// 1. Character input lo
-// var char = prompt("Enter a character:");
-
-// // 2. ASCII code find karo
-// var asciiCode = char.charCodeAt(0);
-
-// // 3. Check karo ki character kya hai
-// if (asciiCode >= 48 && asciiCode <= 57) {
-//     document.write("The character is a number.");
-// } else if (asciiCode >= 65 && asciiCode <= 90) {
-//     document.write("The character is an uppercase letter.");
-// } else if (asciiCode >= 97 && asciiCode <= 122) {
-//     document.write("The character is a lowercase letter.");
-// } else {
-//     document.write("The character is not a number or letter.");
+//     alert("Not a number or string");
 // }
 
-var num1 = parseInt(prompt("Enter the first Integer:"))
-var num2 = parseInt(prompt("Enter the second Integer:"))
 
-if (num1 > num2) {
-    document.write("The larger number is" + num1);
-} else if (num2 > num1) {
-    document.write("The larger number is" + num2);
 
-} else {
-    document.write("Both numbers are Equal.");
-}
+
+// QUESTION NO # 2
+// let num1 = Number(prompt("Enter the first Integer:"))
+// let num2 = Number(prompt("Enter the second Integer:"))
+
+// if (num1 > num2) {
+//     document.write("The larger number is " + num1);
+// } else if (num2 > num1) {
+//     document.write("The larger number is " + num2);
+
+// } else {
+//     document.write("Both numbers are Equal.");
+// }
+
+// QUEStION NO # 3
+// let newNum = Number(prompt("Enter the Number:"))
+
+// if (newNum > 0) {
+//     document.write("Positive Number");
+// } else if (newNum < 0) {
+//     document.write("Negetive Number");
+
+// } else if(newNum === 0) {
+//     document.write("Zero");
+// } else if(isNaN(newNum)) {
+//     document.write("Not a Valid number.");
+// }
+
+
+// QUEStION NO # 4
+// let newChar = prompt("Enter a Character");
+// let vowels = "aeiouAEIOU";
+
+// if(vowels.includes(newChar)) {
+//     alert(true);
+// } else {
+//     alert(false);
+// }
+
+// QUEStION NO # 5
+// let correctPassword = "pakistan123";
+// let checkPassword = prompt("Enter your password");
+
+// if(checkPassword === "") {
+//     alert("Please enter your password");
+// } else if(checkPassword === correctPassword) {
+//     alert("“Correct! The password you entered matches the original password");
+// } else {
+//     alert("Incorrect password");
+// }
+
+// QUEStION NO # 6
+// var greeting;
+// var hour = 1;
+// if (hour < 18) {
+// alert(greeting = "Good day");
+// }
+// else {
+// alert(greeting = "Good evening");
+// }
+
+// QUEStION NO # 7
+// let time = Number(prompt("Enter time"));
+
+// if (time >= 0 && time <= 11) {
+//     alert("Good Morning!");
+// } else if (time >= 12 && time <= 16) {
+//     alert("Good Afternoon");
+// } else if (time >= 17 && time <= 20) {
+//     alert("Good Evening");
+// } else if(time >= 21 && time <= 23) {
+//     alert("Good Night");
+// } else {
+//     alert("Invalid type input");
+// }
