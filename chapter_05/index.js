bffe1893b6c76230b2f46b0afe5a81e172405b46
muhammad saad <studentsaad41@gmail.com>
2024-09-15 22:59:39 +0500

    completed

--- chapter_05/index.js
+++ chapter_05/index.js
@@ -1,45 +1,57 @@
 // QUESTION 1
 var num1 = 30, num2 = 50;
 var numNew = num1 + num2;
-document.write(`<h3>Sum of ${num1} and ${num2} is ${numNew}</h3><br/>`);
+document.write(`<p>Sum of ${num1} and ${num2} is ${numNew}</p><br/>`);
 // document.write("Sum of " + num1 + " and " + num2 + " is " + numNew );
 
 // QUESTION 2
 var num1 = 100, num2 = 50;
 var numNew = num1 - num2;
-document.write(`<h3>Subtract ${num1} from ${num2} is ${numNew}</h3>`);
+document.write(`<p>Subtract ${num1} from ${num2} is ${numNew}</p>`);
 
 var num1 = 40, num2 = 10;
 var numNew = num1 * num2;
-document.write(`<h3>Multiply ${num1} from ${num2} is ${numNew}</h3>`);
+document.write(`<p>Multiply ${num1} from ${num2} is ${numNew}</p>`);
 
 var num1 = 1400, num2 = 5;
 var numNew = num1 / num2;
-document.write(`<h3>${num1} divided by ${num2} is ${numNew}</h3>`);
+document.write(`<p>${num1} divided by ${num2} is ${numNew}</p>`);
 
 var num1 = 100, num2 = 90;
 var numNew = num1 % num2;
-document.write(`<h3>Modulus ${num1} of ${num2} is ${numNew}</h3><br/>`);
+document.write(`<p>Modulus ${num1} of ${num2} is ${numNew}</p><br/>`);
 
 // QUESTION 3
 var newVariable;
-document.write(`<h3>Value after variable declaration is: ${newVariable}</h3>`);
+document.write(`<p>Value after variable declaration is: ${newVariable}</p>`);
 newVariable = 5;
-document.write(`<h3>Initial value: ${newVariable}</h3>`);
+document.write(`<p>Initial value: ${newVariable}</p>`);
 newVariable++;
-document.write(`<h3>Value after increament is: ${newVariable}</h3>`);
+document.write(`<p>Value after increament is: ${newVariable}</p>`);
 newVariable += 7;
-document.write(`<h3>Value after addition is: ${newVariable}</h3>`);
+document.write(`<p>Value after addition is: ${newVariable}</p>`);
 newVariable--;
-document.write(`<h3>Value after decreament is: ${newVariable}</h3>`);
+document.write(`<h3>Value after decreament is: ${newVariable}</p>`);
 var reminder = newVariable % 3;
-document.write(`<h3>The reminder is: ${reminder}</h3>`);
+document.write(`<p>The reminder is: ${reminder}</p>`);
 
 // QUESTION 4
 var oneTicket = 600, fiveTickets = oneTicket * 5;
 document.write(`Total cost to buy 5 tickets to a movie is ${fiveTickets} PKR <br><br><br>`);
 
 // QUESTION 5
+
+let tableOf = Number(prompt("Enter a Number ")) 
+document.write(`<h1>Table of ${tableOf} </h1> `);
+
+let multiplyBy = 10;
+
+
+for (let i = 1; i <= multiplyBy; i++) {
+    let result = tableOf * i;
+    document.write(`<p>${tableOf} x ${i} = ${result} </p>`);
+    
+}
 
 
 // QUESTION 6
@@ -99,7 +111,7 @@ document.write("Total Currency in PKR: " + totalPKR + "<br/>");
 // QUESTION 10
 var randomNum = 10;
 var result = (randomNum + 5) * 10 / 2;
-document.write("<h1>Question No. 10</h1>" + "<p>Think a Number:</p>" + result); 
+document.write("<h1>Question No. 10</h1>" + "<p>Think a Number: I'll calculate</p>" + result); 
 
 // QUESTION 11
 var currentYear = 2024;
@@ -110,3 +122,27 @@ var calcAge = currentYear - birthYear;
 document.write("<h1>Age Calculator</h1> <p>Current Year: " + currentYear + "</p><p>Birth Year: " + birthYear + "</p><p>Your age is : " + calcAge + "</p>" );
 
 // QUESTION 12
+// let radius = Number(prompt("radius"));
+let radius = 20;
+let PI = 3.142;
+let circumference = 2 * PI * radius;
+let area = PI * radius ** 2;
+
+
+document.write(`<h1>The Geometrizer </h1>`)
+document.write(`<p>Radius of a circle is: ${radius} </p>`)
+document.write(`<p>The circumference is: ${circumference} </p>`)
+document.write(`<p>The area is: ${area} </p>`)
+
+// QUESTION 13
+let fvrt = "Ice cream";
+let age = 22;
+let maxAge = 60;
+let perday = 2;
+let total = perday * maxAge - perday * age;
+document.write(`<h1>The Lifetime Supply Calculator </h1> `) 
+document.write(`<p>Favorite thing: ${fvrt} </p> `) 
+document.write(`<p>Current age: ${age} </p> `) 
+document.write(`<p>Estimated maximum age: ${maxAge} </p> `) 
+document.write(`<p>Amount of snacks per day: ${perday} </p> `) 
+document.write(`<p>You will need ${total} to last you until the ripe old age of ${maxAge} </p> `) 
